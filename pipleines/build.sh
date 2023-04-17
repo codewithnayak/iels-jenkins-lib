897925d00d8ba5e9be8d9bb1954fcc2a8348b8af
codewithnayak <codewithnayak@gmail.com>
2023-04-17 17:23:40 +0100

    added params

--- pipleines/build.sh
+++ pipleines/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-
-echo '*** build started'
+COMMANDNAME=$1
+LISTOFNUMBERS=$2
+echo '*** build started***'
 echo 'something will be built here'
+
+if [ $COMMANDNAME=="high" ]; then
+    echo "highfi command"
+else
+    echo "low standard command"
+fi
+
+echo 'Display list of numbers'
+for name in ${LISTOFNUMBERS[@]}; do
+    echo ${name}
+done
 echo '*** build ends ****'
